1651f3ba9dfd7d5bf3f33873a38675dcc867006a
halspring <hal.webworks@gmail.com>
2017-03-31 23:30:09 +0900

    静的ページ用コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+
+  get 'pages/index'
+
+  get 'user/index'
+
+  get 'user/show'
+
   resources :scenarios
   resources :sessions
   devise_for :users
