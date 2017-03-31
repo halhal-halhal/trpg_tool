9e9ca87600bd902e2a8ea311f7ef9653d2b184ec
halspring <hal.webworks@gmail.com>
2017-03-31 23:07:45 +0900

    Userモデル周辺の変更 varidation一部、登録フォーム変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name_id, uniqueness: true
+  validates :name, :name_id, presence: true
+
   has_many :followings
   has_many :users, :through => :followings
   has_many :participations
